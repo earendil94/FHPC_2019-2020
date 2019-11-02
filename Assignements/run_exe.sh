2b153dc9a24b12dc76b45140e689a2e4d50583cf
Francesco Brand <francesco.brand94@gmail.com>
2019-11-02 20:41:02 +0100

    Run to be extended to weak scaling for serial algo. Report to be written.

--- Assignements/run_exe.sh
+++ Assignements/run_exe.sh
@@ -6,7 +6,9 @@ run_serial(){
 	#echo $1
 	#echo $2
 	#echo $3
-	time ./$3 $1 > $2
+	#echo "$3 $1"
+	#{ time ./$3 $1 ; } 2> $2
+	time ./$3 $1 
 }
 
 #Pass p as first argument, n as second argument, result file as third argument, program as fourth argument
@@ -14,17 +16,29 @@ run_parallel(){
 	time mpirun -np $1 $4 $2 > $3
 }
 
-#Pass filename_p as first argument, string to grep as second argument, csv name as third
+#Pass filename_p as first argument, csv name as second argument
 #Our csv will have these columns: Rank,P,Time,N
 result_files_to_csv(){
 
-	P=$(echo $1 | cut -d"_" -f2)
-	N=$(echo $1 | cut -d"_" -f3)
+	export P=$(echo $1 | cut -d"_" -f3)
+	export N=$(echo $1 | cut -d"_" -f4)
 	#In case we are extracting walltime from serial app
+
+	if [[ $CURR_PROGRAM == *"PartialSum"* ]]
+	then
+		serial_sum_elab $1 $2
+	fi
+
 	if (( $P == 1 ))
 	then
 		WALLTIME=$(grep "walltime" $1 | cut -d":" -f2)
-		echo "${P};${P};${WALLTIME};${N}" >> $2
+		if [[ $WALLTIME == "" ]]
+		then
+			USERTIME=$(grep "user" $1 | cut -d"m" -f2 | cut -d"s" -f1)
+			echo "${P};${P};${USERTIME};${N}" >> $2
+		else
+			echo "${P};${P};${WALLTIME};${N}" >> $2
+		fi
 	else
 		for ((rank=0;rank<$P;rank++))
 		do
@@ -39,42 +53,84 @@ result_files_to_csv(){
 	fi
 }
 
+#First argument: file_name; Second argument: csv_name
+serial_sum_elab(){
+	T_READ=$(grep "T_read" $1 | cut -d":" -f2)
+	T_COMM=$(grep "T_comm" $1 | cut -d":" -f2)
+	T_COMP=$(grep "T_comp" $1 | cut -d":" -f2)
+
+	echo "T_READ;${P};${T_READ};${N}" >> $2.times
+	echo "T_COMM;${P};${T_COMM};${N}" >> $2.times
+	echo "T_COMP;${P};${T_COMP};${N}" >> $2.times
+}
+
 CURR_PROGRAM=$1
 SCALING_TEST=$2
+
+if [[ $2 != "WEAK" ]]
+then
+	SCALING_TEST=STRONG
+fi
+
 
 if [[ $CURR_PROGRAM == *"mpi"* ]]
 then
 	module load openmpi
 fi
 
-P_VALUES=$(grep P_VALUES_{$SCALING_TEST} values.cfg | cut -d"=" -f2)
-N_VALUES=$(grep N_VALUES_{$SCALING_TEST} values.cfg | cut -d"=" -f2)
-RESULT_FILE=$(echo $CURR_PROGRAM | cut -d"." -f1)_{$SCALING_TEST}.res
-CSV_FILE=$(echo $CURR_PROGRAM | cut -d"." -f1)_{$SCALING_TEST}.csv
+P_VALUES=$(grep P_VALUES_${SCALING_TEST} values.cfg | cut -d"=" -f2)
+N_VALUES=$(grep N_VALUES_${SCALING_TEST} values.cfg | cut -d"=" -f2)
+RESULT_FILE=$(echo $CURR_PROGRAM | cut -d"." -f1)_${SCALING_TEST}.res
+CSV_FILE=$(echo $CURR_PROGRAM | cut -d"." -f1)_${SCALING_TEST}.csv
 
 #echo $CURR_PROGRAM
-if [[ $SCALING_TEST == "STRONG" ]]
+
+if [[ $CURR_PROGRAM == *"mpi"* ]]
 then
-	for n_val in $N_VALUES
-	do
-		for p_val in $P_VALUES
+	if [[ $SCALING_TEST == "STRONG" ]]
+	then
+		for n_val in $N_VALUES
 		do
-			if [[ $CURR_PROGRAM == *"mpi"* ]]
-			then
+			for p_val in $P_VALUES
+			do
 				run_parallel $p_val $n_val ${RESULT_FILE}_${p_val}_${n_val} $CURR_PROGRAM
 				result_files_to_csv ${RESULT_FILE}_${p_val}_${n_val} $CSV_FILE
 				rm ${RESULT_FILE}_${p_val}_${n_val}
-			else
-				run_serial $n_val ${RESULT_FILE}_${p_val}_${n_val} $CURR_PROGRAM
-				result_files_to_csv ${RESULT_FILE}_${p_val}_${n_val} $CSV_FILE
-				rm ${RESULT_FILE}_${p_val}_${n_val}
-			fi
+			done
 		done
-	done
+	elif [[ $SCALING_TEST == "WEAK" ]]
+	then
+		for p_val in $P_VALUES
+		do
+			n_val=$(( p_val * $N_VALUES ))
+			run_parallel $p_val $n_val ${RESULT_FILE}_${p_val}_${n_val} $CURR_PROGRAM
+			result_files_to_csv ${RESULT_FILE}_${p_val}_${n_val} $CSV_FILE
+			rm ${RESULT_FILE}_${p_val}_${n_val}
+		done
+	fi
 else
-	for p_val in $P_VALUES
+	if [[ $SCALING_TEST == "STRONG" ]]
+	then
+		for n_val in $N_VALUES
+		do
+			run_serial $n_val ${RESULT_FILE}_1_${n_val} $CURR_PROGRAM
+			result_files_to_csv ${RESULT_FILE}_1_${n_val} $CSV_FILE
+			rm ${RESULT_FILE}_1_${n_val}
+		done
+	elif [[ $SCALING_TEST == "WEAK" ]]
+	then
+		for p_val in $P_VALUES
+		do
+			n_val=$(( p_val * $N_VALUES ))
+			run_serial $n_val ${RESULT_FILE}_${p_val}_${n_val} $CURR_PROGRAM
+			result_files_to_csv ${RESULT_FILE}_${p_val}_${n_val} $CSV_FILE
+			rm ${RESULT_FILE}_${p_val}_${n_val}
+		done
+	fi
+fi
+
 
 #Use millisecond instead of second
 awk 'BEGIN { FS=";" } {printf"%d;%d;%.7f;%d\n", $1, $2, $3*1000, $4}' $CSV_FILE > ${CSV_FILE}_out
-#rm $CSV_FILE
-#mv ${CSV_FILE}_out $CSV_FILE
+rm $CSV_FILE
+mv ${CSV_FILE}_out $CSV_FILE
