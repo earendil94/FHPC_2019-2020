33ec6cfa3c8e4c3f4423c871290623fb57b18c5a
Francesco Brand <francesco.brand@gmail.com>
2019-12-08 18:55:09 +0100

    Added print different cores spotting on prefixSum

--- Assignements/Assignment02/prefixSum.c
+++ Assignements/Assignment02/prefixSum.c
@@ -3,6 +3,7 @@
 #     define _XOPEN_SOURCE 700
 #  endif
 #endif
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -23,6 +24,12 @@
 
 #endif
 
+#ifdef OUTPUT
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
 #define DEFAULT 8
 
 int main(int argc, char **argv){
@@ -34,7 +41,22 @@ int main(int argc, char **argv){
     if(argc > 1)
         n = atoi(*(argv+1));
 
-    
+      // just give notice of what will happen and get the number of threads used
+    #ifndef _OPENMP
+      printf("serial prefix sum\n");
+    #else
+    #pragma omp parallel
+    {
+    #pragma omp master
+        {
+        nthreads = omp_get_num_threads();
+        printf("omp prefix sum with %d threads\n", nthreads );
+        }
+        int me = omp_get_num_threads();
+    #pragma omp critical
+        PRINTF("thread %2d is running on core %2d\n", me, get_cpu_id() ); 
+    }
+    #endif
 
     if( (arr = (double *) malloc(n*sizeof(double))) == NULL ){
         printf("Malloc failed, exiting program\n");
@@ -138,4 +160,81 @@ int main(int argc, char **argv){
     //     printf("%f\t",arr[k]);
     // }
 
+}
+
+
+int get_cpu_id( void )
+{
+#if defined(_GNU_SOURCE)                              // GNU SOURCE ------------
+  
+  return  sched_getcpu( );
+
+#else
+
+#ifdef SYS_getcpu                                     //     direct sys call ---
+  
+  int cpuid;
+  if ( syscall( SYS_getcpu, &cpuid, NULL, NULL ) == -1 )
+    return -1;
+  else
+    return cpuid;
+  
+#else      
+
+  unsigned val;
+  if ( read_proc__self_stat( CPU_ID_ENTRY_IN_PROCSTAT, &val ) == -1 )
+    return -1;
+
+  return (int)val;
+
+#endif                                                // -----------------------
+#endif
+
+}
+
+
+
+int read_proc__self_stat( int field, int *ret_val )
+/*
+  Other interesting fields:
+
+  pid      : 0
+  father   : 1
+  utime    : 13
+  cutime   : 14
+  nthreads : 18
+  rss      : 22
+  cpuid    : 39
+
+  read man /proc page for fully detailed infos
+ */
+{
+  // not used, just mnemonic
+  // char *table[ 52 ] = { [0]="pid", [1]="father", [13]="utime", [14]="cutime", [18]="nthreads", [22]="rss", [38]="cpuid"};
+
+  *ret_val = 0;
+
+  FILE *file = fopen( "/proc/self/stat", "r" );
+  if (file == NULL )
+    return -1;
+
+  char   *line = NULL;
+  int     ret;
+  size_t  len;
+  ret = getline( &line, &len, file );
+  fclose(file);
+
+  if( ret == -1 )
+    return -1;
+
+  char *savetoken = line;
+  char *token = strtok_r( line, " ", &savetoken);
+  --field;
+  do { token = strtok_r( NULL, " ", &savetoken); field--; } while( field );
+
+  *ret_val = atoi(token);
+
+  free(line);
+
+  return 0;
 }
