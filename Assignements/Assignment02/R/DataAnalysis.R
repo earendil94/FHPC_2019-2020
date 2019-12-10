b53aacab3825c06081227b7d13fa282b1a4b18fd
Francesco Brand <francesco.brand@gmail.com>
2019-12-10 21:57:51 +0100

    R Analysis ready for data

--- Assignements/Assignment02/R/DataAnalysis.R
+++ Assignements/Assignment02/R/DataAnalysis.R
@@ -14,18 +14,11 @@ library(dplyr)
 setwd("C:/BashShared/FHPC_2019-2020/Assignements/Assignment02/R")
 
 #Reading files
-#df.01 <- read.csv("../01_array_sum.csv", sep = ";")
-#df.06 <- read.csv("../06_touch_by_all.csv", sep = ";")
 df.01 <- read.csv("../../ulyssesResults/01_array_sum.csv", sep = ";")
 df.06 <- read.csv("../../ulyssesResults/06_touch_by_all.csv", sep = ";")
 df.preSum <- read.csv("../../ulyssesResults/prefixSum.csv", sep = ";")
 
 #Filtering my stuff
-
-str(df.01)
-str(df.06)
-
-df.01
 
 
 #StrongScaling
@@ -61,9 +54,46 @@ strongScaling <- function(df){
   #do.call(grid.arrange, plots)
 }
 
+parallelOverhead <- function(df){
+  
+  df.times <- df %>%
+    group_by(N, THREADS) %>%
+    summarise(time = mean(TIME))
+  
+  K <- unique(df.times$N)
+  TH <- unique(df.times$THREADS)
+  TH <- (2:20)
+  
+  par(mfrow=c(1,2))
+  for (n in K){
+    df.temp <- filter(df.times, N == n)
+    T1 <- df.temp %>%
+      filter(THREADS == 1) %>%
+      pull(time)
+    TP <- df.temp %>%
+      filter(THREADS != 1) %>%
+      pull(time)
+    
+    sp <- T1/TP
+    #Let's add speedup for 1 thread manually
+    parallel <- (1/sp - 1/TH)/(1 - 1/TH)
+    
+    data_to_graph <- tibble(Thr= TH, e = parallel)
+    plot(data_to_graph, type="l")
+    
+  }
+  #do.call(grid.arrange, plots)
+}
 
-strongScaling(df.01) #Right now this is less than one
-strongScaling(df.06) #Stops scaling (even though slowly) from 
+
+
+strongScaling(df.01)
+strongScaling(df.06)  
+strongScaling(df.preSum)
+
+parallelOverhead(df.01)
+parallelOverhead(df.preSum)
+
 strongScaling(df.preSum)
 
 
